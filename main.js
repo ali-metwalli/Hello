da669604d00c836fd9aef9b910a74bf34df8d19a
Ali Metwalli <alimetwalli.us@gmail.com>
2022-03-21 12:43:48 -0400

    second draft

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let greetings = ['hello', 'hi', 'hola', 'salam', 'ahlan'];
 let randomGreeting = greetings[getRandomInt(4)];
 console.log(`You can greet someone by saying ${randomGreeting}`);
 
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
